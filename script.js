8f3b81e8bd8d3c88effd9981cd83d90621339e4e
Meow <50887230+itzTheMeow@users.noreply.github.com>
2020-12-09 11:02:59 -0500

    add proper loading items

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window._ = function (id) {
   return document.getElementById(id);
 };
+setInterval(function () {
+  _("content-loader").style.top = _("navbar").offsetHeight;
+  _("content-loader").style.height = _("content").scrollHeight;
+});
